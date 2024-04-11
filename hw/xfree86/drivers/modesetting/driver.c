b37e8b03b57921cf395bc4e04633b40c3fef6b6e
Dongwon Kim <dongwon.kim@intel.com>
2024-04-11 09:35:33 -0700

    modesetting: Empty damage once dispatch is done
    This commit is to fix some regression caused by
    
    commit 995e60a91997a00768517d1ab6b40fc4f78b7cdc
    
    where DamageEmpty on damage was missing in several
    places.
    
    Signed-off-by: Dongwon Kim <dongwon.kim@intel.com>

--- hw/xfree86/drivers/modesetting/driver.c
+++ hw/xfree86/drivers/modesetting/driver.c
@@ -751,6 +751,8 @@ dispatch_dirty(ScreenPtr pScreen)
             return;
         }
     }
+
+    DamageEmpty(ms->damage);
 }
 
 static void
@@ -762,6 +764,8 @@ dispatch_dirty_pixmap(ScrnInfoPtr scrn, xf86CrtcPtr crtc, PixmapPtr ppix)
     int fb_id = ppriv->fb_id;
 
     dispatch_dirty_region(scrn, crtc, ppix, damage, fb_id, 0, 0);
+    if (damage)
+        DamageEmpty(damage);
 }
 
 static void
