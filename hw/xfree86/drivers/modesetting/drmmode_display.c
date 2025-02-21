b563eb7c605fe1099c437ff66b1648b896f0183f
Ville Syrjälä <ville.syrjala@linux.intel.com>
2025-02-21 22:01:53 +0200

    modesetting: Parse the IN_FORMATS_ASYNC blob
    The kernel has gained another format/modifier blob to indicate
    which formats/modifiers support async flips. Parse it.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

--- hw/xfree86/drivers/modesetting/drmmode_display.c
+++ hw/xfree86/drivers/modesetting/drmmode_display.c
@@ -111,8 +111,18 @@ get_opaque_format(uint32_t format)
     }
 }
 
+static drmmode_format_ptr get_format(drmmode_crtc_private_ptr drmmode_crtc,
+                                     Bool async_flip, int i)
+{
+    if (async_flip && drmmode_crtc->formats_async)
+        return &drmmode_crtc->formats_async[i];
+    else
+        return &drmmode_crtc->formats[i];
+}
+
 Bool
-drmmode_is_format_supported(ScrnInfoPtr scrn, uint32_t format, uint64_t modifier)
+drmmode_is_format_supported(ScrnInfoPtr scrn, uint32_t format,
+                            uint64_t modifier, Bool async_flip)
 {
     xf86CrtcConfigPtr xf86_config = XF86_CRTC_CONFIG_PTR(scrn);
     int c, i, j;
@@ -132,7 +142,7 @@ drmmode_is_format_supported(ScrnInfoPtr scrn, uint32_t format, uint64_t modifier
             continue;
 
         for (i = 0; i < drmmode_crtc->num_formats; i++) {
-            drmmode_format_ptr iter = &drmmode_crtc->formats[i];
+            drmmode_format_ptr iter = get_format(drmmode_crtc, async_flip, i);
 
             if (iter->format != format)
                 continue;
@@ -163,7 +173,7 @@ drmmode_is_format_supported(ScrnInfoPtr scrn, uint32_t format, uint64_t modifier
 #ifdef GBM_BO_WITH_MODIFIERS
 static uint32_t
 get_modifiers_set(ScrnInfoPtr scrn, uint32_t format, uint64_t **modifiers,
-                  Bool enabled_crtc_only, Bool exclude_multiplane)
+                  Bool enabled_crtc_only, Bool exclude_multiplane, Bool async_flip)
 {
     xf86CrtcConfigPtr xf86_config = XF86_CRTC_CONFIG_PTR(scrn);
     modesettingPtr ms = modesettingPTR(scrn);
@@ -183,7 +193,7 @@ get_modifiers_set(ScrnInfoPtr scrn, uint32_t format, uint64_t **modifiers,
             continue;
 
         for (i = 0; i < drmmode_crtc->num_formats; i++) {
-            drmmode_format_ptr iter = &drmmode_crtc->formats[i];
+            drmmode_format_ptr iter = get_format(drmmode_crtc, async_flip, i);
 
             if (iter->format != format)
                 continue;
@@ -238,7 +248,8 @@ get_drawable_modifiers(DrawablePtr draw, uint32_t format,
         return TRUE;
     }
 
-    *num_modifiers = get_modifiers_set(scrn, format, modifiers, TRUE, FALSE);
+    *num_modifiers = get_modifiers_set(scrn, format, modifiers,
+                                       TRUE, FALSE, FALSE);
     return TRUE;
 }
 #endif
@@ -2287,7 +2298,7 @@ is_plane_assigned(ScrnInfoPtr scrn, int plane_id)
  */
 static Bool
 populate_format_modifiers(xf86CrtcPtr crtc, const drmModePlane *kplane,
-                          uint32_t blob_id)
+                          drmmode_format_rec *formats, uint32_t blob_id)
 {
     drmmode_crtc_private_ptr drmmode_crtc = crtc->driver_private;
     drmmode_ptr drmmode = drmmode_crtc->drmmode;
@@ -2333,9 +2344,9 @@ populate_format_modifiers(xf86CrtcPtr crtc, const drmModePlane *kplane,
             modifiers[num_modifiers - 1] = mod->modifier;
         }
 
-        drmmode_crtc->formats[i].format = blob_formats[i];
-        drmmode_crtc->formats[i].modifiers = modifiers;
-        drmmode_crtc->formats[i].num_modifiers = num_modifiers;
+        formats[i].format = blob_formats[i];
+        formats[i].modifiers = modifiers;
+        formats[i].num_modifiers = num_modifiers;
     }
 
     drmModeFreePropertyBlob(blob);
@@ -2351,7 +2362,7 @@ drmmode_crtc_create_planes(xf86CrtcPtr crtc, int num)
     drmModePlaneRes *kplane_res;
     drmModePlane *kplane, *best_kplane = NULL;
     drmModeObjectProperties *props;
-    uint32_t i, type, blob_id;
+    uint32_t i, type, blob_id, async_blob_id;
     int current_crtc, best_plane = 0;
 
     static drmmode_prop_enum_info_rec plane_type_enums[] = {
@@ -2374,6 +2385,7 @@ drmmode_crtc_create_planes(xf86CrtcPtr crtc, int num)
         [DRMMODE_PLANE_FB_ID] = { .name = "FB_ID", },
         [DRMMODE_PLANE_CRTC_ID] = { .name = "CRTC_ID", },
         [DRMMODE_PLANE_IN_FORMATS] = { .name = "IN_FORMATS", },
+        [DRMMODE_PLANE_IN_FORMATS_ASYNC] = { .name = "IN_FORMATS_ASYNC", },
         [DRMMODE_PLANE_SRC_X] = { .name = "SRC_X", },
         [DRMMODE_PLANE_SRC_Y] = { .name = "SRC_Y", },
         [DRMMODE_PLANE_SRC_W] = { .name = "SRC_W", },
@@ -2448,6 +2460,8 @@ drmmode_crtc_create_planes(xf86CrtcPtr crtc, int num)
             best_kplane = kplane;
             blob_id = drmmode_prop_get_value(&tmp_props[DRMMODE_PLANE_IN_FORMATS],
                                              props, 0);
+            async_blob_id = drmmode_prop_get_value(&tmp_props[DRMMODE_PLANE_IN_FORMATS_ASYNC],
+                                                   props, 0);
             drmmode_prop_info_copy(drmmode_crtc->props_plane, tmp_props,
                                    DRMMODE_PLANE__COUNT, 1);
             drmModeFreeObjectProperties(props);
@@ -2459,6 +2473,8 @@ drmmode_crtc_create_planes(xf86CrtcPtr crtc, int num)
             best_kplane = kplane;
             blob_id = drmmode_prop_get_value(&tmp_props[DRMMODE_PLANE_IN_FORMATS],
                                              props, 0);
+            async_blob_id = drmmode_prop_get_value(&tmp_props[DRMMODE_PLANE_IN_FORMATS_ASYNC],
+                                                   props, 0);
             drmmode_prop_info_copy(drmmode_crtc->props_plane, tmp_props,
                                    DRMMODE_PLANE__COUNT, 1);
         } else {
@@ -2473,9 +2489,18 @@ drmmode_crtc_create_planes(xf86CrtcPtr crtc, int num)
         drmmode_crtc->num_formats = best_kplane->count_formats;
         drmmode_crtc->formats = calloc(best_kplane->count_formats,
                                        sizeof(drmmode_format_rec));
-        if (!populate_format_modifiers(crtc, best_kplane, blob_id)) {
+        if (!populate_format_modifiers(crtc, best_kplane,
+                                       drmmode_crtc->formats, blob_id)) {
             for (i = 0; i < best_kplane->count_formats; i++)
                 drmmode_crtc->formats[i].format = best_kplane->formats[i];
+        } else {
+            drmmode_crtc->formats_async = calloc(best_kplane->count_formats,
+                                                 sizeof(drmmode_format_rec));
+            if (!populate_format_modifiers(crtc, best_kplane,
+                                           drmmode_crtc->formats_async, async_blob_id)) {
+                free(drmmode_crtc->formats_async);
+                drmmode_crtc->formats_async = NULL;
+            }
         }
         drmModeFreePlane(best_kplane);
     }
