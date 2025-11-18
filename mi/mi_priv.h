c8c10e7198b45b4940f32b6a8dc18c8c8969683a
kohnish <kohnish@gmx.com>
2025-11-18 21:40:41 +0100

    Revert "mi: unexport miWideArc()"
    This reverts commit aaaa0a35e5981b77f5bc9303799db566e746b600.

--- mi/mi_priv.h
+++ mi/mi_priv.h
@@ -5,15 +5,8 @@
 #ifndef _XSERVER_MI_PRIV_H
 #define _XSERVER_MI_PRIV_H
 
-#include <X11/Xprotostr.h>
-
-#include "include/gc.h"
-#include "include/pixmap.h"
-#include "include/screenint.h"
-#include "mi/mi.h"
+#include "screenint.h"
 
 void miScreenClose(ScreenPtr pScreen);
-
-void miWideArc(DrawablePtr pDraw, GCPtr pGC, int narcs, xArc * parcs);
 
 #endif /* _XSERVER_MI_PRIV_H */
